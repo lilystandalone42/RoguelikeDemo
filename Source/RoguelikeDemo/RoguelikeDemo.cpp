9ecdf3124ffb40d47cad732819a3fe95c9d554ab
Lily Yao <lily.stand.alone.42@gmail.com>
2026-07-25 15:17:55 +0800

    Initial project from UE 5.4 Top Down template

---
+++ Source/RoguelikeDemo/RoguelikeDemo.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RoguelikeDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RoguelikeDemo, "RoguelikeDemo" );
+
+DEFINE_LOG_CATEGORY(LogRoguelikeDemo)
+ 
